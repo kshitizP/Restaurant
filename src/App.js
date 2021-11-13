a18044da867ac033868b76718ed7d8ca93e6f4c5
Shubham <getshubhk@gmail.com>
2021-11-13 14:02:34 +0530

    Fixes for Responsiveness

--- src/App.js
+++ src/App.js
@@ -7,19 +7,7 @@ function App() {
 
   return (
     <div  className="App">
-      <nav id="nav-main" role="navigation" >
-        <ul className="nav">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#content-menu">Menu</a></li>
-          <li><a href="#">Reservations</a></li>
-          <li><a href="#">Private Events</a></li>
-          <li><a href="#">Contact Us</a></li>
-          <li><a href="#">Social</a></li>
-        </ul>
-      </nav>
       <section className="bgd" style={{backgroundImage: `url(${landing_image})`}}>
-        <header className="welcome-text">Welcome to Our Restaurant</header>
         <div className="menu-anchor">
           <p>Today's Menu</p>
           <p className="down-button"><a href='#content-menu'><i className="arrow down"></i></a></p>
